9f4577b45437da5138a001e32d69db0767158c2c
Sergio Cruz <sergioing@yahoo.com>
2025-06-24 00:30:43 -0500

    Solving warnings mesages 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Header1 from "./components/header1.jsx";
-import Options1 from "./components/options1.jsx";
-import Spinner from "./components/spinner.jsx";
 import CarouselSelector from "./components/carouselselector.jsx";
 import './App.css';
 import "leaflet/dist/leaflet.css";
